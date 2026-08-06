65d853dec1e46368fd912d64626c30c9cbb9144a
Joseph Hefner <Hefnerjoseph87@gmail.com>
2026-08-06 16:51:29 -0700

    Refactor data directory creation and API token usage

--- R/get_data.R
+++ R/get_data.R
@@ -1,4 +1,3 @@
-
 library(httr2)
 library(readr)
 
@@ -6,25 +5,30 @@ library(readr)
 
 YEAR <- 2025
 
-# Create year-specific data directory if necessary 
-data_dir <- file.path("Data", YEAR) 
+# Create year-specific data directory if necessary
+data_dir <- file.path("Data", YEAR)
 
-if (!dir.exists(data_dir)) { 
-  dir.create(data_dir, recursive = TRUE) 
+if (!dir.exists(data_dir)) {
+  dir.create(data_dir, recursive = TRUE)
 }
 
 # Get private FTN data -----
 
 load_ftn_private <- function(type = "all22", year = YEAR) {
-  
   url <- paste0(
     "https://api.github.com/repos/",
     "josephjefe/ftn_data/contents/",
-    "Data/", year, "/", type, ".csv"
+    "Data/",
+    year,
+    "/",
+    type,
+    ".csv"
   )
-  
+
   httr2::request(url) |>
-    httr2::req_auth_bearer_token(Sys.getenv("FTN_DATA_PAT")) |>
+    httr2::req_auth_bearer_token(Sys.getenv("FTN_DATA_2025")) |>
+    # Use if in RStudio
+    # httr2::req_auth_bearer_token(Sys.getenv("FTN_PAT_2025")) |>
     httr2::req_headers(
       Accept = "application/vnd.github.raw+json"
     ) |>
@@ -39,4 +43,5 @@ load_ftn_private <- function(type = "all22", year = YEAR) {
 all22_raw <- load_ftn_private(
   type = "all22",
   year = YEAR
-)
+) |>
+  janitor::clean_names()
