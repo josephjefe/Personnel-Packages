9bbacca69f454c2247ce18413f34458dc783a496
Joseph Hefner <21974312+josephjefe@users.noreply.github.com>
2026-09-15 17:14:41 -0700

    Uncomment bearer token authentication for requests

--- R/get_data.R
+++ R/get_data.R
@@ -27,9 +27,9 @@ load_ftn_private <- function(type = "all22", year = YEAR) {
   )
 
   httr2::request(url) |>
-    # httr2::req_auth_bearer_token(Sys.getenv("FTN_DATA_PAT")) |>
+    httr2::req_auth_bearer_token(Sys.getenv("FTN_DATA_PAT")) |>
     # Use if in RStudio
-    httr2::req_auth_bearer_token(Sys.getenv("FTN_PAT_2025")) |>
+    # httr2::req_auth_bearer_token(Sys.getenv("FTN_PAT_2025")) |>
     httr2::req_headers(
       Accept = "application/vnd.github.raw+json"
     ) |>
